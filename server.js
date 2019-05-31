95e2cbf8b1bcb7fd954852727ac6bdb7028e5c22
fabianiba <fabian08_04hhotmail.com>
2019-05-31 18:49:28 -0300

    TEST

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -8,8 +9,6 @@ connectDB();
 
 //Init Middleware
 app.use(express.json({extended: false}));
-
-app.get('/', (req, res) => res.send('Hello MADAFACA'));
 
 
 //Define Routes
@@ -24,6 +23,16 @@ app.use('/api/proyect-type', require('./routes/api/projectType'));
 
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
+
+//Configuraciones para produccion
+if(process.env.NODE_ENV === "production"){
+    //server static 
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
