1abf45f1b86143f4f83e2e9ee8d486701a1c4769
fabianiba <fabian08_04hhotmail.com>
2019-06-20 19:59:30 -0300

    control de la edición de usuarios

--- client/src/components/admin/AdminCreateUser.js
+++ client/src/components/admin/AdminCreateUser.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import {setAlert} from '../../actions/alert';
+import moment from 'moment';
 import {registerUser, editUser} from '../../actions/user';
 
 
@@ -29,6 +30,20 @@ const AdminCreateUser = ({match, editUser, setAlert, registerUser, history, user
         for (let index = 0; index < users.length; index++) {
             if(users[index]._id == match.params.idUser){
                 var userEdit = users[index];
+
+                const fecha = new Date(userEdit.birth);
+    
+                let mes = fecha.getMonth()+1;
+                if(mes<10) mes='0'+mes;
+
+                let dia = fecha.getDate();
+                if(dia<10) dia='0'+dia
+
+                let anio = fecha.getFullYear();
+                var cumple = `${anio}-${mes}-${dia}`
+
+                userEdit.birth = cumple
+
             }
         }
     }
@@ -42,7 +57,7 @@ const AdminCreateUser = ({match, editUser, setAlert, registerUser, history, user
             surname: loading || !userEdit.surname ? '' : userEdit.surname,
             name: loading || !userEdit.name ? '' : userEdit.name,
             cuil: loading || !userEdit.cuil ? '' : userEdit.cuil,
-            birth: loading || !userEdit.birth ? '' : userEdit.birth,
+            birth: loading || !userEdit.birth ? '' :  userEdit.birth,
             address: loading || !userEdit.address ? '' : userEdit.address,
             province: loading || !userEdit.province ? '' : userEdit.province,
             phone: loading || !userEdit.phone ? '' : userEdit.phone,
@@ -76,6 +91,34 @@ const AdminCreateUser = ({match, editUser, setAlert, registerUser, history, user
         
     }
 
+    const divPass = (
+        <div className="form-group">
+            <h4>Ingrese la contraseña</h4>
+            <input
+                type="password"
+                placeholder="Contraseña"
+                name="pass"
+                minLength="6"
+                onChange = {e => onChange(e)}
+                value={pass}
+            />
+        </div>
+    )
+
+    const divRepeatPass = (
+        <div className="form-group">
+            <h4>Repetir la contraseña</h4>
+            <input
+                type="password"
+                placeholder="Confirmar contraseña"
+                name="repeatPass"
+                minLength="6"
+                onChange = {e => onChange(e)}
+                value={repeatPass}
+            />
+        </div>
+    )
+
   return (
     <Fragment>
         
@@ -88,6 +131,7 @@ const AdminCreateUser = ({match, editUser, setAlert, registerUser, history, user
         <form className="form" onSubmit={e => onSubmit(e)}>
 
             <div className="form-group">
+                <h4>Ingrese apellido</h4>
                 <input 
                     type="text" 
                     placeholder="Apellido" 
@@ -98,6 +142,7 @@ const AdminCreateUser = ({match, editUser, setAlert, registerUser, history, user
             </div>
             
             <div className="form-group">
+                <h4>Ingrese nombre</h4>
                 <input 
                     type="text" 
                     placeholder="Nombre" 
@@ -108,6 +153,7 @@ const AdminCreateUser = ({match, editUser, setAlert, registerUser, history, user
             </div>
 
             <div className="form-group">
+                <h4>Ingrese CUIL</h4>
                 <input 
                     type="text" 
                     placeholder="CUIL" 
@@ -118,6 +164,7 @@ const AdminCreateUser = ({match, editUser, setAlert, registerUser, history, user
             </div>
 
             <div className="form-group">
+                <h4>Ingrese Fecha de nacimiento</h4>
                 <input 
                     type="date" 
                     placeholder="" 
@@ -125,10 +172,10 @@ const AdminCreateUser = ({match, editUser, setAlert, registerUser, history, user
                     value={birth}
                     onChange = {e => onChange(e)}
                 />
-                <small className="form-text">Fecha de nacimiento</small>
             </div>
 
             <div className="form-group">
+                <h4>Ingrese dirección</h4>
                 <input 
                     type="text" 
                     placeholder="Dirección" 
@@ -139,8 +186,9 @@ const AdminCreateUser = ({match, editUser, setAlert, registerUser, history, user
             </div>
 
             <div className="form-group">
+                <h4>Seleccione la provincia</h4>
                 <select name="province" value={province} onChange = {e => onChange(e)}>
-                    <option value="">* Seleccione la provincia</option>
+                    <option value="">* Elija</option>
                     <option value="Buenos Aires">Buenos Aires</option>
                     <option value="Catamarca">Catamarca</option>
                     <option value="Chaco">Chaco</option>
@@ -168,6 +216,7 @@ const AdminCreateUser = ({match, editUser, setAlert, registerUser, history, user
             </div>
 
             <div className="form-group">
+                <h4>Ingrese el Teléfono</h4>
                 <input 
                     type="text" 
                     placeholder="Teléfono" 
@@ -178,44 +227,38 @@ const AdminCreateUser = ({match, editUser, setAlert, registerUser, history, user
             </div>
 
             <div className="form-group">
+                <h4>Seleccione el rol</h4>
                 <select name="rol" value={rol} onChange = {e => onChange(e)}>
-                    <option value="">* Seleccione el rol</option>
-                    <option value="Admin">Administrador</option>
-                    <option value="Operativo">Operativo</option>
+                    <option value="">* Elija</option>
+                    <option value="Admin">Administrador General de Sistema</option>
+                    <option value="Responsable de Proyecto">Responsable de Proyecto</option>
+                    <option value="Integrante de Equipo de Proyecto">Integrante de Equipo de Proyecto</option>
+                    <option value="Supervisor">Supervisor de Programa</option>
+                    <option value="Cliente">Cliente</option>
                 </select>
             </div>
 
             <div className="form-group">
-            <input 
-                type="email" 
-                placeholder="Email"
-                onChange = {e => onChange(e)} 
-                name="email"
-                value={email}
-            />
+                <h4>Ingrese el email</h4>
+                <input 
+                    type="email" 
+                    placeholder="Email"
+                    onChange = {e => onChange(e)} 
+                    name="email"
+                    value={email}
+                />
             </div>
+
+            { match.params.idUser != undefined ? null : divPass }
             
-            <div className="form-group">
-            <input
-                type="password"
-                placeholder="Contraseña"
-                name="pass"
-                minLength="6"
-                onChange = {e => onChange(e)}
-                value={pass}
-            />
-            </div>
-            <div className="form-group">
-            <input
-                type="password"
-                placeholder="Confirmar contraseña"
-                name="repeatPass"
-                minLength="6"
-                onChange = {e => onChange(e)}
-                value={repeatPass}
-            />
-            </div>
-            <input type="submit" className="btn btn-primary" value="Registrar" />
+            { match.params.idUser != undefined ? null : divRepeatPass }
+
+            <Link to="/admin-user" className="btn btn-danger">
+                Cancelar
+            </Link>
+
+            <input type="submit" className="btn btn-primary" value={ match.params.idUser != undefined ? "Editar" : "Registrar" } />
+             
         </form>
     </Fragment>
   )
