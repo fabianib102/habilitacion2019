e86667977ae174706d32055b09de5031d5d5d679
andres <andresj.sanchez@hotmail.com>
2019-10-16 19:17:35 -0300

    Revert "se agrega el maquetado de la vista de integrante de equipo"
    This reverts commit 3e77e40e6ad7acd520c59ab7ffbe6e6191e99e77.

--- client/src/App.js
+++ client/src/App.js
@@ -73,8 +73,6 @@ import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
-import TeamMember from './components/teamMember/TeamMember';
-import teamMemberDetail from './components/teamMember/teamMemberDetail';
 
 if(localStorage.token){
   setAuthToken(localStorage.token);
@@ -155,9 +153,6 @@ const App = () => {
               <PrivateRoute exact path="/admin-project/create-project" component={AdminCreateProject} />
               <PrivateRoute exact path="/admin-project/project-activity/:idProject" component={AdminProjectActivity} />
               
-              <PrivateRoute exact path="/team-member" component={TeamMember} />
-              <PrivateRoute exact path="/team-member/team-member-detail" component={teamMemberDetail} />
-
               {/* <PrivateRoute exact path="/proyect" component={Project} /> */}
 
               <PrivateRoute exact path="/create-task" component={CreateTask} />
